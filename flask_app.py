7ef873cbebf4182356de65c6e72d155070da4d5b
DPrashu <prashudeshmukh3006@gmail.com>
2025-03-10 17:20:40 +0530

    HTML files

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,12 @@
 from flask import Flask,render_template,request,session,jsonify,redirect
+import numpy as np,pandas as pd,pickle
+from datetime import date
+import datetime
+
+model = pickle.load(open('AdaBoost.pkl','rb'))
+CountryCodes = pickle.load(open('Country_Codes.pkl','rb'))
+dataSet = pickle.load(open('Customer_dataset.pkl','rb'))
+DataScaler = pickle.load(open('scaler.pkl','rb'))
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"  # Needed for session management
@@ -52,6 +60,34 @@ def logged_in():
 @app.route('/logout')
 def logout():
     return redirect('/')
+
+@app.route('/submit',methods=['POST'])
+def submit():
+    customerId = float(request.form.get('customer_id'))
+    country = request.form.get('country')
+    country_code = CountryCodes[country]
+    monetary_str = request.form.get('transaction_amount', '0').strip()
+    monetary = ''.join(filter(str.isdigit, monetary_str))
+    monetary = float(monetary)
+    last_visit = request.form.get('last_visit') 
+    last_visit = datetime.datetime.strptime(last_visit, "%Y-%m-%d").date()
+    frequency = float(request.form.get('total_visits'))
+    recency = (date.today() - last_visit).days
+
+
+    arr = np.array([recency,frequency,monetary,country_code])
+    arr = arr.reshape(1,-1)
+    arr = DataScaler.transform(arr)
+    cluster_id = model.predict(arr)
+
+    if(cluster_id == 0):
+        return render_template('cluster0.html')
+    if(cluster_id == 1):
+        return render_template('cluster1.html')
+    if(cluster_id == 2):
+        return render_template('cluster2.html')
+    if(cluster_id == 3):
+        return render_template('cluster3.html')
 
 if(__name__ == '__main__'):
     app.run(debug=True)
